48af3e824331dd83f821afeffc903ee9f4496295
osagie obaretin <osagie.codes@gmail.com>
2020-06-07 05:37:54 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,16 +2,13 @@
 
 def my_all?(collection)
   i = 0
-  block_return_value = []
+  block_return_values = []
   while i < collection.length
-    block_return_value <<
-yield(collection[i])
-    i = i + 1
+    block_return_values << yield(collection[i])
+    i += 1
   end
-  
-  if 
-block_return_value.include?
-(false)
+
+  if block_return_values.include?(false)
     false
   else
     true
